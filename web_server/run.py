c68a12a22a59cfadd4dbc48f47a196165ba2c0b6
Anton Kachurin <katchuring@gmail.com>
2019-07-19 01:32:52 +0300

    Fixing tests in CI

--- web_server/run.py
+++ web_server/run.py
@@ -10,7 +10,20 @@ from web_server.api import SERVER
 from web_server.configuration import CONFIGURATION
 from web_server.database import init_db
 
-PID_FILE = os.path.abspath("/run/web-server.pid")
+
+def _pid_dir_permissions():
+    default = "/run"
+    file_name = f"{default}/randomfilename"
+    try:
+        open(file_name, "w+").close()
+        os.remove(file_name)
+        return True
+    except IOError:
+        return False
+
+
+PID_DIR = os.path.abspath("/run" if _pid_dir_permissions() else os.path.abspath("."))
+PID_FILE = os.path.abspath(f"{PID_DIR}/web-server.pid")
 
 
 def main(action, debug):
@@ -21,12 +34,6 @@ def main(action, debug):
     if action == "start":
         init_db()
         with daemon.DaemonContext(pidfile=PIDLockFile(PID_FILE), detach_process=True):
-            if not os.path.exists(PID_FILE):
-                print("NO PID FILE EXISTS")
-                with open(PID_FILE, "w+") as pid:
-                    pid.write(str(os.getpid()))
-            else:
-                print("PID FILE EXISTS")
             WSGIServer(SERVER, port=int(os.getenv("SERVER_PORT", CONFIGURATION.SERVER_PORT))).start()
     else:
         with open(PID_FILE) as pid_file:
