947acc8c92b4e6f79238502782e6b38ef9660f03
Anton Kachurin <anton.kachurin@t-systems.com>
2019-09-04 14:54:35 +0300

    Fixing not starting server

--- too_simple_server/run.py
+++ too_simple_server/run.py
@@ -1,5 +1,4 @@
 """Server control"""
-
 import os
 import signal
 
@@ -11,24 +10,31 @@ from .configuration import DEFAULT_CFG_PATH, load_configuration
 from .database import init_db
 
 
-def _pid_dir():
-    default = "/tmp/too-simple"
-    fallback = os.path.abspath("~/.too-simple")
-    os.makedirs(default, exist_ok=True)
+def _try_dir(default, fallback):
     file_name = f"{default}/randomfilename"
     try:
+        os.makedirs(default, exist_ok=True)
         open(file_name, "w+").close()
         os.remove(file_name)
         return default
-    except IOError:
+    except (IOError, PermissionError):
         os.makedirs(fallback, exist_ok=True)
         return fallback
 
 
+def _pid_dir():
+    return _try_dir("/tmp/too-simple", os.path.expanduser("~/.too-simple"))
+
+
+def _log_dir():
+    return _try_dir("/var/log/too-simple", os.path.expanduser("~/.too-simple"))
+
+
 PID_FILE = os.path.abspath(f"{_pid_dir()}/web-server.pid")
+LOG_FILE = os.path.abspath(f"{_pid_dir()}/execution.log")
 
 
-def main(action, debug=None, configuration_path=DEFAULT_CFG_PATH):
+def main(action, debug=None, configuration_path=DEFAULT_CFG_PATH, no_wsgi=False):
     """Start/stop running server"""
     import daemon  # *nix only
 
@@ -43,8 +49,14 @@ def main(action, debug=None, configuration_path=DEFAULT_CFG_PATH):
 
     def _start():
         init_db(configuration)
-        with daemon.DaemonContext(pidfile=PIDLockFile(PID_FILE), detach_process=True):
-            WSGIServer(SERVER, port=configuration.server_port).start()
+        with open(LOG_FILE, "w+") as log_file, \
+                daemon.DaemonContext(pidfile=PIDLockFile(PID_FILE),
+                                     detach_process=True,
+                                     stdout=log_file, stderr=log_file):
+            if no_wsgi:
+                SERVER.run("0.0.0.0", configuration.server_port)
+            else:
+                WSGIServer(SERVER, port=configuration.server_port).start()
 
     if action == "start":
         _start()
