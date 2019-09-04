ae107ca66f8f68afa6572377854007b7a9ca666f
Anton Kachurin <anton.kachurin@t-systems.com>
2019-09-04 15:00:03 +0300

    Fixing not starting server

--- too_simple_server/__main__.py
+++ too_simple_server/__main__.py
@@ -8,7 +8,8 @@ AGP = ArgumentParser(description="Mock server with simple DB interactions")
 AGP.add_argument("--debug", action="store_true", default=None)
 AGP.add_argument("--config", help=f"Configuration file to be used, '{DEFAULT_CFG_PATH}' by default",
                  default=DEFAULT_CFG_PATH)
+AGP.add_argument("--no-wsgi", action="store_true", default=False)
 AGP.add_argument("action", default="start", choices=["start", "stop"])
 ARGS = AGP.parse_args()
 
-main(ARGS.action, ARGS.debug, ARGS.config)
+main(ARGS.action, ARGS.debug, ARGS.config, ARGS.no_wsgi)
